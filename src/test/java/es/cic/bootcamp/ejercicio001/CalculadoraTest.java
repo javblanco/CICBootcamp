06f3545c4f0cedfff272a270b03df81bd8dbce98
unknown <usuario@L-2103080.cic.es>
2021-07-08 14:06:03 +0200

    Version inicial de un proyecto con un test unitario

---
+++ src/test/java/es/cic/bootcamp/ejercicio001/CalculadoraTest.java
@@ -0,0 +1,29 @@
+package es.cic.bootcamp.ejercicio001;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSumar() {
+		Calculadora calculadora = new Calculadora();
+		
+		int a = 4;
+		int s2 = 5;
+		
+		int resultado = calculadora.sumar(a,s2); 
+		
+		assertEquals(9,resultado, "La suma no es correcta");
+		
+	}
+}
+
+
+
